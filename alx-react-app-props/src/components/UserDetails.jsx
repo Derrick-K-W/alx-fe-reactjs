14847a032106e9f62e5b043f2f98883fa09ff9ba
Derrick-K-W <derrickkwanyoike@gmail.com>
2024-10-22 20:40:23 +0300

    Added new files

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,16 @@
+// UserDetails.jsx
+import { useContext } from 'react';
+import UserContext from './UserContext'; // Import UserContext
+
+function UserDetails() {
+  const userData = useContext(UserContext); // Consume the context
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
