e485e21e2040f05a658c4d9cdcf1b6d2a100a5e9
Derrick-K-W <derrickkwanyoike@gmail.com>
2024-10-24 00:36:50 +0300

    Modified Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError("Looks like we can't find the user"); // Ensure correct phrasing
     } finally {
       setLoading(false);
     }
