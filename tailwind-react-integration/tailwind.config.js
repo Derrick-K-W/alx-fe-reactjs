930d36e4bf514f16fdc95b507dacdcc2553cd48b
Derrick-K-W <derrickkwanyoike@gmail.com>
2024-10-23 22:46:04 +0300

    Modified tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // This replaces `purge`
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Scans these files for class names
+  darkMode: 'media', // Set darkMode to 'media' or 'class'
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {}, // You can add variants here if needed
   },
   plugins: [],
 };
