57634a0a7c8594afc4b0b8113ff72902764294c6
Derrick-K-W <derrickkwanyoike@gmail.com>
2024-10-23 22:55:18 +0300

    Made changes to src folder

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import UserProfile from './components/UserProfile';
 
 export default function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello Tailwind CSS
-      </h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <UserProfile />
     </div>
   );
 }
