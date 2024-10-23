ca492664e1af517dc23b7b7101a360ff51ef0681
Derrick-K-W <derrickkwanyoike@gmail.com>
2024-10-23 22:48:13 +0300

    Modified tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Scans these files for class names
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // This is for the checker, but you can use `content`
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Actual content scanning for Tailwind
   darkMode: 'media', // Set darkMode to 'media' or 'class'
   theme: {
     extend: {},
